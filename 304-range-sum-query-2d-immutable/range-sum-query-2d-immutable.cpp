1ae84328e77593ae2816f6d650f76ae9f42d583b
nik123-hue <nishantkumar0123.jeh@gmail.com>
2026-02-05 00:44:47 +0530

    Time: 21 ms (66.70%) | Memory: 149.2 MB (57.38%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
@@ -8,20 +8,29 @@ vector <vector<int>>mat;
                 mat[i][j]+=mat[i][j-1];
             }
         }
+        for(int i=1;i<matrix.size();i++){
+            for(int j=0;j<matrix[0].size();j++){
+                mat[i][j]+=mat[i-1][j];
+            }
+        }
 
         
     }
     
     int sumRegion(int row1, int col1, int row2, int col2) {
-        int sum=0;
-        for(int i=row1;i<=row2;i++){
-            if(col1>0){
-                sum += mat[i][col2]-mat[i][col1-1];
-            }
-            else{
-                sum+=mat[i][col2];
-            }
+        int sum =0;
+        int top_sum=0,left_sum=0,topleft_sum=0;
+        if(row1!=0){
+            top_sum=mat[row1-1][col2];
         }
+        if(col1!=0){
+            left_sum=mat[row2][col1-1];
+        }
+        if(row1!=0 && col1!=0){
+            topleft_sum=mat[row1-1][col1-1];
+        }
+        sum=mat[row2][col2]-top_sum-left_sum+topleft_sum;
+        
         return sum;
         
     }
