0087ba22d1cb985085657e3a4d33ebd2aa703f94
nik123-hue <nishantkumar0123.jeh@gmail.com>
2026-02-05 00:46:38 +0530

    Time: 24 ms (56.30%) | Memory: 149.1 MB (86.60%) - LeetSync

--- 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
+++ 304-range-sum-query-2d-immutable/range-sum-query-2d-immutable.cpp
@@ -20,13 +20,13 @@ vector <vector<int>>mat;
     int sumRegion(int row1, int col1, int row2, int col2) {
         int sum =0;
         int top_sum=0,left_sum=0,topleft_sum=0;
-        if(row1!=0){
+        if(row1>0){
             top_sum=mat[row1-1][col2];
         }
-        if(col1!=0){
+        if(col1>0){
             left_sum=mat[row2][col1-1];
         }
-        if(row1!=0 && col1!=0){
+        if(row1>0 && col1>0){
             topleft_sum=mat[row1-1][col1-1];
         }
         sum=mat[row2][col2]-top_sum-left_sum+topleft_sum;
