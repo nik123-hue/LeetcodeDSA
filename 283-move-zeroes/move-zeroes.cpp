e134c0f482d25985150f310025f1a785bdcd9b4e
nik123-hue <nishantkumar0123.jeh@gmail.com>
2026-02-22 15:38:14 +0530

    Time: 0 ms (100.00%) | Memory: 23.9 MB (54.91%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& v) {
-        int n= v.size();
-        for(int i=n-1;i>=0;i--){
-            int j=0;
-            bool flag=false;
-            while(j!=i){
-                if(v[j]==0 && v[j+1]!=0){
-                    swap(v[j],v[j+1]);
-                    flag = true;
-                }
-                j++;
+        int pos = 0;
+        for (int i=0; i<v.size(); i++) {
+            if (v[i] != 0) {
+                v[pos++] = v[i];
             }
-            if(!flag) break;
         }
-        return;
+        while (pos < v.size()) {
+            v[pos++] = 0;
+        }
     }
 };
