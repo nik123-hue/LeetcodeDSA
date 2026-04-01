a778e5569e1c0df82712b73176f30c97d81b87a4
nik123-hue <nishantkumar0123.jeh@gmail.com>
2026-04-02 01:40:14 +0530

    Time: 0 ms (100.00%) | Memory: 65.1 MB (68.49%) - LeetSync

---
+++ 287-find-the-duplicate-number/find-the-duplicate-number.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) { // cyclic sort method
+        int n= nums.size();
+        int i= 0;
+        while(i<n){
+            int correctIdx=nums[i];
+            if(nums[correctIdx]==nums[i]) return nums[i];
+            else swap(nums[i],nums[correctIdx]);
+        }
+         return 1000;
+        
+    }
+};
