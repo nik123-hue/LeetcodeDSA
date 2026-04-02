75ae51929e5c0ac3e917b940042dde2a4b3673be
nik123-hue <nishantkumar0123.jeh@gmail.com>
2026-04-02 14:58:17 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (81.40%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
@@ -4,7 +4,7 @@ public:
         int sum=0;
         for (int i=0;i<nums.size();i++){
             sum += nums[i];
-            nums[i]=sum;
+            nums[i]= sum;
         }
               return nums;   
     }
