a2a906b80220b2ed12855b134bce6f2aa98d5d62
nik123-hue <nishantkumar0123.jeh@gmail.com>
2026-04-06 15:49:38 +0530

    Time: 2 ms (67.16%) | Memory: 17.7 MB (87.54%) - LeetSync

--- 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.cpp
@@ -9,12 +9,19 @@ public:
             nums[i] += nums[i-1];  
         }
         for(int i=0;i<m;i++){
-            int len = 0;
-            for(int j=0;j<n;j++){
-                if(nums[j]>queries[i]) break;
-                len++;
+            int maxLen = 0;
+            int lo = 0;
+            int hi = n-1;
+            while(lo<=hi){
+                int mid = lo + (hi-lo)/2;
+                if(nums[mid]>queries[i]) hi = mid - 1;
+                else{
+                    maxLen = mid + 1;
+                    lo = mid + 1;
+                }
             }
-               ans[i] = len;
+            
+               ans[i] = maxLen;
         }
          return ans;
         
