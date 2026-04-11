60aa1a1c908bc56b383944bdeecf594e564b6fb6
nik123-hue <nishantkumar0123.jeh@gmail.com>
2026-04-11 23:58:24 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (69.31%) - LeetSync

--- 1503-reducing-dishes/reducing-dishes.cpp
+++ 1503-reducing-dishes/reducing-dishes.cpp
@@ -7,13 +7,12 @@ public:
         suf[n-1] = satisfaction[n-1];
         for(int i = n-2;i>=0;i--){
             suf[i] = suf[i+1] + satisfaction[i];
-
         }
         int idx =-1;
         for(int i=0;i<n;i++){
             if(suf[i]>=0){
                 idx = i;
-                break;
+                 break;
             }
         }
         if(idx==-1) return 0;
@@ -23,7 +22,6 @@ public:
             maxSum += (satisfaction[i]*x);
             x++;
         }
-         return maxSum;
-        
+         return maxSum;      
     }
 };
