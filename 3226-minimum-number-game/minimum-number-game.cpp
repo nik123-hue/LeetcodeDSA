34ebb2679caeb3c7b5c5f62062f7799d4aa8468d
nik123-hue <nishantkumar0123.jeh@gmail.com>
2026-04-18 01:26:24 +0530

    Time: 0 ms (100.00%) | Memory: 28.3 MB (33.38%) - LeetSync

--- 3226-minimum-number-game/minimum-number-game.cpp
+++ 3226-minimum-number-game/minimum-number-game.cpp
@@ -3,9 +3,12 @@ public:
     vector<int> numberGame(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(),nums.end());
+        vector<int> ans;
         for(int i=0;i<n;i+=2){
-            swap(nums[i],nums[i+1]);
+            ans.push_back(nums[i+1]);
+            ans.push_back(nums[i]);
+            
         }
-        return nums;     
+        return ans;     
     }
 };
