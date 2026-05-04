62170768ba0904a7db614f1e772eac262db8f357
nik123-hue <nishantkumar0123.jeh@gmail.com>
2026-05-04 22:58:06 +0530

    Time: 13 ms (19.20%) | Memory: 11.9 MB (21.68%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -8,9 +8,6 @@ public:
             fast = fast->next->next;
             if(slow==fast) return true;
         }
-        return false;
-        
-        
-        
+        return false;    
     }
 };
