6d1dcada8daf89b54f82698663923e6690b78a0b
nik123-hue <nishantkumar0123.jeh@gmail.com>
2026-06-18 18:40:16 +0530

    Time: 0 ms (100.00%) | Memory: 10.3 MB (16.65%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -7,36 +7,32 @@ stack<int> helper;
     }
     
     void push(int x) {
-        st.push(x);
-        
-    }
-    
-    int pop() {
-        while(st.size()>0){
+        if(st.size()==0){
+            st.push(x);
+            return;
+        }
+        else {
+             while(st.size()>0){
             helper.push(st.top());
             st.pop();
         }
-        int x = helper.top();
-        helper.pop();
+        st.push(x);
         while(helper.size()>0){
             st.push(helper.top());
             helper.pop();
         }
+      }   
+    }
+    
+    int pop() {
+        int x = st.top();
+        st.pop();
         return x;
         
     }
     
     int peek() {
-         while(st.size()>0){
-            helper.push(st.top());
-            st.pop();
-        }
-        int x = helper.top();
-        while(helper.size()>0){
-            st.push(helper.top());
-            helper.pop();
-        }
-        return x;      
+         return st.top(); 
     }
     
     bool empty() {
