7407742db8dcbed97dc91771fc9692849a60b15b
nik123-hue <nishantkumar0123.jeh@gmail.com>
2026-07-13 16:19:41 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (44.70%) - LeetSync

---
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.cpp
@@ -0,0 +1,27 @@
+
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        if(root==NULL) return 0;
+        int lh = findHeightLeft(root);
+        int rh = findHeightRight(root);
+        if(lh==rh) return (1<<lh)-1;
+        return 1 + countNodes(root->left) + countNodes (root->right);   
+    }
+     int findHeightLeft(TreeNode* node){
+        int height = 0;
+        while(node){
+            height++;
+            node = node->left;
+        }
+        return height;
+     }
+     int findHeightRight(TreeNode* node){
+        int height = 0;
+        while(node){
+            height++;
+            node = node->right;
+        }
+        return height;
+     }
+};
