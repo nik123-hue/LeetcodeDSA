6beb3d47bea608aeb22efe256fc58a166bb70a26
nik123-hue <nishantkumar0123.jeh@gmail.com>
2026-07-27 00:10:49 +0530

    Time: 1 ms (50.45%) | Memory: 13.9 MB (85.15%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -1,8 +1,9 @@
 class Solution {
     public:
     void findCombination(int idx,int target,vector<int> &arr,vector<vector<int>> &ans,vector<int> &ds){
-        if(target==0){ ans.push_back(ds);
-        return ;
+        if(target==0){ 
+            ans.push_back(ds);
+        return;
         }
         for(int i = idx;i<arr.size();i++){
             if(i>idx && arr[i]==arr[i-1]) continue;
