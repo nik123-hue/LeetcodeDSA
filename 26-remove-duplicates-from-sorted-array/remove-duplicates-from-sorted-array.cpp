ec01109f061233307705497ff3af164d48f4e5e0
nik123-hue <nishantkumar0123.jeh@gmail.com>
2026-08-16 16:52:33 +0530

    Time: 0 ms (100.00%) | Memory: 22.7 MB (53.41%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,15 +1,21 @@
 class Solution {
 public:
-    int removeDuplicates(vector<int>& arr) {
-        int n = arr.size();
-        int i = 0;
-        for(int j=1;j<n;j++){
-            if(arr[i] != arr[j]){
-                i++;
-                arr[i] = arr[j];
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        int j=0 , k = 0 , i = 0;
+        while(j < n){
+            if(nums[i] == nums[j]){
+                j++;
             }
-        }
-        return i+1;
+            else{
+                nums[k] = nums[i];
+                k++;
+                i = j;
+            }
+
         
+        }
+        nums[k++] = nums[n-1];
+        return k;
     }
 };
