08cc6aef2d1975558dddbac697c074ff67afd0f0
nik123-hue <nishantkumar0123.jeh@gmail.com>
2026-09-01 00:26:19 +0530

    Time: 65 ms (30.58%) | Memory: 51 MB (19.02%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int,int> mpp;
+        mpp[0] = 1;
         int n = nums.size();
+        int preSum = 0;
         int count = 0;
         for(int i=0;i<n;i++){
-            int sum = 0;
-            for(int j=i;j<n;j++){
-                 sum += nums[j];
-                    if(sum==k)
-                    count++;  
-            }
+          preSum += nums[i];
+          int remove = preSum - k;
+          count += mpp[remove];
+          mpp[preSum] += 1;
         }
         return count;
     }
