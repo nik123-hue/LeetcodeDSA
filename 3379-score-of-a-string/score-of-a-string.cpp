863b27f65bf083e7668c577f1b1ee3eadaa01aba
nik123-hue <nishantkumar0123.jeh@gmail.com>
2026-09-04 23:25:14 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (30.59%) - LeetSync

--- 3379-score-of-a-string/score-of-a-string.cpp
+++ 3379-score-of-a-string/score-of-a-string.cpp
@@ -2,7 +2,8 @@ class Solution {
 public:
     int scoreOfString(string s) {
         int sum = 0;
-        for(int i=0;i<s.size()-1;i++){
+        int n = s.size();
+        for(int i=0;i<n-1;i++){
            sum += abs((int)s[i]-(int)s[i+1]);
         }
         return sum;
