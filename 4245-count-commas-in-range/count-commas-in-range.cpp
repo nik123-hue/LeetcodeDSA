4313adee5acf0c11039fc382bc1a608784b0ccff
nik123-hue <nishantkumar0123.jeh@gmail.com>
2026-09-08 14:25:58 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (84.49%) - LeetSync

--- 4245-count-commas-in-range/count-commas-in-range.cpp
+++ 4245-count-commas-in-range/count-commas-in-range.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
     int countCommas(int n) {
-        int total = 0;
-        int a = n-999;
-        if(a>0) total += a;
-        return total;
+        return max(n-999,0);
     }
 };
