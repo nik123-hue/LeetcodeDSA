1e738f6c00c4eb3a1cbacc45d3fc783f35210182
nik123-hue <nishantkumar0123.jeh@gmail.com>
2026-09-12 23:58:33 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (11.34%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool possible(vector<int>& nums, int k, int maxSum) {
+        int subarrays = 1;
+        long long sum = 0;
+        for (int num : nums) {
+            if (sum + num <= maxSum) {
+                sum += num;
+            } 
+            else {
+                subarrays++;
+                sum = num;
+            }
+        }
+        return subarrays <= k;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int low = *max_element(nums.begin(), nums.end());
+        long long high = 0;
+        for (int num : nums)
+            high += num;
+        long long ans = high;
+        while (low <= high) {
+            long long mid = low + (high - low) / 2;
+            if (possible(nums, k, mid)) {
+                ans = mid;
+                high = mid - 1;
+            } 
+            else {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
