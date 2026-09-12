ef04c85663838d0173aa13a65de29994e1679fbb
nik123-hue <nishantkumar0123.jeh@gmail.com>
2026-09-13 00:08:44 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (73.76%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.cpp
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -1,35 +1,36 @@
 class Solution {
 public:
-    bool possible(vector<int>& nums, int k, int maxSum) {
+    int countSubarrays(vector<int>& nums, long long maxSum) {
         int subarrays = 1;
         long long sum = 0;
-        for (int num : nums) {
-            if (sum + num <= maxSum) {
-                sum += num;
-            } 
+        for (int i = 0; i < nums.size(); i++) {
+            if (sum + nums[i] <= maxSum) {
+                sum += nums[i];
+            }
             else {
                 subarrays++;
-                sum = num;
+                sum = nums[i];
             }
         }
-        return subarrays <= k;
+        return subarrays;
     }
     int splitArray(vector<int>& nums, int k) {
-        int low = *max_element(nums.begin(), nums.end());
-        int high = 0;
-        for (int num : nums)
-            high += num;
-        long long ans = high;
+
+       int low = *max_element(nums.begin(), nums.end());
+       int high = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            high += nums[i];
+        }
         while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (possible(nums, k, mid)) {
-                ans = mid;
+           int mid = low + (high - low) / 2;
+            int subarrays = countSubarrays(nums, mid);
+            if (subarrays <= k) {
                 high = mid - 1;
-            } 
+            }
             else {
                 low = mid + 1;
             }
         }
-        return ans;
+        return low;
     }
 };
