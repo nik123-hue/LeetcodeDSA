66d3e8b92a679d2dbc051c110af75d8f2ce000ef
nik123-hue <nishantkumar0123.jeh@gmail.com>
2026-09-17 00:06:17 +0530

    Time: 3 ms (22.36%) | Memory: 15.8 MB (63.82%) - LeetSync

--- 2283-sort-even-and-odd-indices-independently/sort-even-and-odd-indices-independently.cpp
+++ 2283-sort-even-and-odd-indices-independently/sort-even-and-odd-indices-independently.cpp
@@ -1,30 +1,21 @@
 class Solution {
 public:
     vector<int> sortEvenOdd(vector<int>& nums) {
-        int n = nums.size();
-        int e = n / 2;
-        if (n % 2 == 1)
-            e = (n / 2) + 1;
-        vector<int> even(e);
-        vector<int> odd(n / 2);
-        int j = 0, k = 0;
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                even[j++] = nums[i];
-            }
-            else {
-                odd[k++] = nums[i];
-            }
+        vector<int> even, odd;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i % 2 == 0)
+                even.push_back(nums[i]);
+            else
+                odd.push_back(nums[i]);
         }
         sort(even.begin(), even.end());
-        sort(odd.begin(), odd.end());
-        j = 0;
-        k = (n / 2) - 1;
-        for (int i = 0; i < n; i += 2) {
-            nums[i] = even[j++];
-        }
-        for (int i = 1; i < n; i += 2) {
-            nums[i] = odd[k--];
+        sort(odd.begin(), odd.end(), greater<int>());
+        int j = 0, k = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (i % 2 == 0)
+                nums[i] = even[j++];
+            else
+                nums[i] = odd[k++];
         }
         return nums;
     }
