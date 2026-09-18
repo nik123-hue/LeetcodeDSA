d642a343576e0481fa18621440b9441b507c360a
nik123-hue <nishantkumar0123.jeh@gmail.com>
2026-09-18 23:26:02 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (12.77%) - LeetSync

--- 231-power-of-two/power-of-two.cpp
+++ 231-power-of-two/power-of-two.cpp
@@ -4,11 +4,8 @@ public:
         if(n<=0)
             return false;
             while(n%2==0){
-                 n/=2;
-            
+                 n/=2;   
         }
-        return n==1;
-
-        
+        return n==1;   
     }
 };
